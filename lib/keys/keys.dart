f5ce5471547e0ff6108bfa540fe6a22de8a22ee1
satoshi-sh <satoss1108@gmail.com>
2026-04-05 17:23:29 -0500

    Finished 152 understanding and using keys and finished chapter flutter internal

--- lib/keys/keys.dart
+++ lib/keys/keys.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internals/keys/checkable_todo_item.dart';
 
-import 'package:flutter_internals/keys/todo_item.dart';
+import 'package:flutter_internals/keys/todo_item.dart' hide Priority;
 
 class Todo {
   const Todo(this.text, this.priority);
@@ -69,9 +70,10 @@ class _KeysState extends State<Keys> {
             children: [
               // for (final todo in _orderedTodos) TodoItem(todo.text, todo.priority),
               for (final todo in _orderedTodos)
-                TodoItem(
+                CheckableTodoItem(
                   todo.text,
                   todo.priority,
+                  key: ValueKey(todo.text),
                 ),
             ],
           ),
